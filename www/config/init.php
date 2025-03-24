5da5e9aa2aefcbf49dcf9a2178f06c9992d03de0
A1exFox <alex.fox.personal@gmail.com>
2025-03-24 22:22:45 +0300

    #1.03 set autoload and init config

---
+++ www/config/init.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+define("DEBUG", 1);
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . "/public");
+define("APP", ROOT . "/app");
+define("CORE", ROOT . "/vendor/wfm");
+define("HELPERS", ROOT . "/vendor/wfm/helpers");
+define("CACHE", ROOT . "/tmp/cache");
+define("LOGS", ROOT . "/tmp/logs");
+define("CONFIG", ROOT . "/config");
+define("LAYOUT", 'ishop');
+define("PATH", 'http://localhost');
+define("ADMIN", 'http://localhost/admin');
+define("NO_IMAGE", 'uploads/no-image.jpg');
+
+require_once ROOT . "/vendor/autoload.php";
--- www/public/index.php
+++ www/public/index.php
@@ -1,45 +1,9 @@
 <?php
 
-phpinfo();
-//xdebug_info();
+declare(strict_types=1);
 
+if (PHP_MAJOR_VERSION < 8) {
+    die("You have to use php version more than 8.0.0");
+}
 
-/* 
-echo "<h2>Test write:</h2>";
-mkdir('./testDir');
-file_put_contents('./testDir/test.txt', 'test', FILE_APPEND);
-exec('rm -rf ./testDir');
- */
-
-
-/* 
-echo "<h2>Test Xdebug:</h2>";
-$a = 3;
-$b = 4;
-$c = $a + $b;
-echo $c;
- */
-
-
-/* 
-echo "<h2>Test mysqli:</h2>";
-$mysqli = new mysqli("mysql", "root", "root", "information_schema");
-var_dump($mysqli->query("SELECT * FROM tables"));
- */
-
-
-/* 
-echo "<h2>Test PDO:</h2>";
-$user = "root";
-$pass = "root";
-$dsn = "mysql:host=mysql;dbname=information_schema;charset=utf8";
-$pdo = new PDO($dsn, $user, $pass);
-$stmp = $pdo->query("SELECT * FROM tables");
-$row = $stmp->fetch();
-print_r($row);
- */
-
-
-// "make dump" in cli to dump db
-
-// "make help" to test composer
+require_once dirname(__DIR__) . "/config/init.php";
