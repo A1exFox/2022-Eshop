47287c0f7a2ded233ed0e67faf3b7427b220a830
A1exFox <alex.fox.personal@gmail.com>
2025-04-03 19:25:08 +0300

    #2.02 setup html layout

--- www/app/views/layouts/ishop.php
+++ www/app/views/layouts/ishop.php
@@ -1,10 +1,9 @@
 <?php
 
 /** @var wfm\View $this */
-
 ?>
-<?php $this->getPart('parts/header'); ?>
+<?php $this->getPart('parts/header') ?>
 
 <?= $this->content ?>
 
-<?php $this->getPart('parts/footer');
+<?php $this->getPart('parts/footer'); ?>
