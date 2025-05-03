4538b8977ab1aa5c01a20ec5eaf1151a59666eb3
A1exFox <alex.fox.personal@gmail.com>
2025-05-03 21:07:35 +0300

    #2.07 Multilanguage#3

--- www/config/routes.php
+++ www/config/routes.php
@@ -7,5 +7,5 @@ use wfm\Router;
 Router::add('^admin/?$', ['controller' => 'Main', 'action' => 'index', 'admin_prefix' => 'admin']);
 Router::add('^admin/(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$', ['admin_prefix' => 'admin']);
 Router::add('^(?<lang>[a-z]+)?/?product/(?<slug>[a-z0-9-]+)/?$', ['controller' => 'Product', 'action' => 'view']);
-Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
+Router::add('^(?P<lang>[a-z]+)?/?$', ['controller' => 'Main', 'action' => 'index']);
 Router::add('^(?P<controller>[a-z-]+)/(?P<action>[a-z-]+)/?$');
--- www/vendor/wfm/helpers/functions.php
+++ www/vendor/wfm/helpers/functions.php
@@ -17,3 +17,19 @@ function h(string $str): string
     $str = htmlspecialchars($str);
     return $str;
 }
+
+function redirect(string $http = ''): void
+{
+    if ($http !== '') {
+        header("Location: $http");
+        die;
+    }
+
+    if (isset($_SERVER['HTTP_REFERER'])) {
+        header("Location: " . $_SERVER['HTTP_REFERER']);
+        die;
+    }
+
+    header("Location: " . PATH);
+    die;
+}
