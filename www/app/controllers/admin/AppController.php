6bf6c56b17c7cccd7150007d177fcb37834d0c97
A1exFox <alex.fox.personal@gmail.com>
2025-05-20 20:50:39 +0300

    #3.01 migration of AdminLTE#1

---
+++ www/app/controllers/admin/AppController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\controllers\admin;
+
+use wfm\Controller;
+
+class AppController extends Controller
+{
+    public string|false $layout = 'admin';
+}
---
+++ www/app/controllers/admin/MainController.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\controllers\admin;
+
+class MainController extends AppController
+{
+    public function indexAction() {}
+}
