d2b0b1e42faa1a805af4cdd623fc5ce67a858675
A1exFox <alex.fox.personal@gmail.com>
2025-05-22 17:17:37 +0300

    #3.06 category#2

--- www/app/controllers/admin/CategoryController.php
+++ www/app/controllers/admin/CategoryController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace app\controllers\admin;
 
+use RedBeanPHP\R;
+
 class CategoryController extends AppController
 {
     public function indexAction()
@@ -11,5 +13,27 @@ class CategoryController extends AppController
         $title = 'Категории';
         $this->setMeta("Админка :: $title");
         $this->set(compact('title'));
+    }
+
+    public function deleteAction()
+    {
+        $id = get('id');
+        $errors = '';
+        $children = R::count('category', 'parent_id = ?', [$id]);
+        $products = R::count('product', 'category_id = ?', [$id]);
+        if ($children) {
+            $errors .= 'Ошибка! Категория содержит вложенные категории<br>';
+        }
+        if ($products) {
+            $errors .= 'Ошибка! Категория содержит товары<br>';
+        }
+        if ($errors) {
+            $_SESSION['errors'] = $errors;
+            redirect();
+        }
+        R::exec("DELETE FROM category WHERE id = ?", [$id]);
+        R::exec("DELETE FROM category_description WHERE category_id = ?", [$id]);
+        $_SESSION['success'] = 'Категория удалена';
+        redirect();
     }
 }
