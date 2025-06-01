222ee0d4106d7cae251858f1b3a3abff3ce872d0
A1exFox <alex.fox.personal@gmail.com>
2025-06-01 14:03:17 +0300

    #3.19 Digital products download

--- www/app/views/admin/parts/footer.php
+++ www/app/views/admin/parts/footer.php
@@ -36,8 +36,9 @@
 <script src="<?= PATH ?>/adminlte/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="<?= PATH ?>/adminlte/dist/js/demo.js"></script>
-<script src="<?= PATH ?>/adminlte/main.js"></script>
 <script src="<?= PATH ?>/adminlte/plugins/select2/js/select2.full.min.js"></script>
+<script src="<?= PATH ?>/adminlte/plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
+<script src="<?= PATH ?>/adminlte/main.js"></script>
 </body>
 
 </html>
