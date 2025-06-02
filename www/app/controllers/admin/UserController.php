15242d0892a8e044f66049c538dac26ba66d4ff8
A1exFox <alex.fox.personal@gmail.com>
2025-06-02 19:09:43 +0300

    #3.23 users listing

--- www/app/controllers/admin/UserController.php
+++ www/app/controllers/admin/UserController.php
@@ -4,11 +4,27 @@ declare(strict_types=1);
 
 namespace app\controllers\admin;
 
+use RedBeanPHP\R;
+use wfm\Pagination;
+
 /**
  * @property \app\models\admin\User $model
  */
 class UserController extends AppController
 {
+    public function indexAction()
+    {
+        $page = get('page');
+        $perpage = 10;
+        $total = R::count('user');
+        $pagination = new Pagination($page, $perpage, $total);
+        $start = $pagination->getStart();
+        $users = $this->model->get_users($start, $perpage);
+        $title = 'Список пользователей';
+        $this->setMeta("Админка::$title");
+        $this->set(compact('title', 'users', 'pagination', 'total'));
+    }
+
     public function loginAdminAction()
     {
         if ($this->model::isAdmin()) {
--- www/app/models/admin/User.php
+++ www/app/models/admin/User.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace app\models\admin;
 
+use RedBeanPHP\R;
+
 class User extends \app\models\User
 {
     public static function isAdmin(): bool
     {
         return isset($_SESSION['user']) && $_SESSION['user']['role'] == 'admin';
+    }
+
+    public function get_users($start, $perpage): array
+    {
+        return R::findAll('user', "LIMIT $start,$perpage");
     }
 }
